326455972f8ddbc8800d3859e4e1381fef48fa77
jiali <mahyarerad@gmail.com>
2025-03-28 20:42:38 +0330

    Defining Constants

---
+++ _inc/define_constants.php
@@ -0,0 +1,15 @@
+<?php 
+
+// Exit if accessed directly
+if (!defined('ABSPATH')) exit;
+// Define plugin directory URI for assets
+define('JST_PLUGIN_URI', plugin_dir_url(dirname(__DIR__) . '/core.php'));
+define('JST_ASSETS_URI', JST_PLUGIN_URI . 'assets');
+define('JST_CSS_URI', JST_ASSETS_URI . '/css');
+define('JST_JS_URI', JST_ASSETS_URI . '/js');
+
+// Define plugin directory path for file inclusion
+define('JST_PLUGIN_PATH', plugin_dir_path(dirname(__DIR__) . '/core.php'));
+define('JST_INC_PATH', JST_PLUGIN_PATH . '_inc');
+
+?>
