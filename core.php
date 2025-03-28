4c40f0458c01f4ebc1f662987e2a04bb706e4e6e
jiali <mahyarerad@gmail.com>
2025-03-28 20:42:17 +0330

    Creating Core

---
+++ core.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: Scroll to Top
+ * Plugin URI: https://mahyarerad.com
+ * Description: A simple plugin for smooth scrolling to top.
+ * Version: 1.0.0
+ * Author: Mahyar Rad
+ * Author URI: https://mahyarerad.com/
+ * License: GPL-2.0+
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: jst
+ * Domain Path: /languages
+ */
+
+
+// Add functions
+require_once( plugin_dir_path(__FILE__) . '/_inc/functions.php' );
+
